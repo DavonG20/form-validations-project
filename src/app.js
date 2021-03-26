34a2dfb467036ca643c44032c83185e6cd45e039
DavonG20 <Grant.Davon@live.jobcorps.org>
2021-03-26 13:18:43 +0000

    initial commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,21 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+function validate(event) {
+  event.preventDefault();
+  var cardNumber = document.querySelector("#inputCardNumber");
+  var amount = document.querySelector("#inputAmount");
+  var firstname = document.querySelector("#inputFirstName");
+  var lastname = document.querySelector("#inputLastName");
+  var state = document.querySelector("#inputState");
+  var zip = document.querySelector("#inputZip");
+
+  if (cardNumber.value.length < 16) {
+  }
+
+  console.log("cardNumber", cardNumber);
+  console.log("cardNumber.value", cardNumber.value);
+  console.log("cardNumber.value.length", cardNumber.value);
+}
+
+document.querySelector("#theForm").addEventListener("submit", validate);
